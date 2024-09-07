3b9463daeb14d725b6f6632916f0efc61f242ea6
VladimirBoro <vborodianski19@gmail.com>
2024-09-06 18:23:20 -0700

    refactor and update+serve image paths added

--- spring/src/main/java/springboot/spring/controller/AccountController.java
+++ spring/src/main/java/springboot/spring/controller/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.google.api.client.util.Value;
@@ -27,17 +28,18 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
-    @GetMapping (path = "/test")
+    @GetMapping ("/test")
     public String test(HttpSession session) {
         return "USER: " + session.getAttribute("username");
     }
 
     @GetMapping("/account/profilePicture")
     public ResponseEntity<Resource> getProfilePicture(@RequestParam String username) {
-        return ResponseEntity.internalServerError().build();
+        System.out.println("getting " + username + "'s pp");
+        return accountService.getProfilePicture(username);
     }
     
-    @PostMapping (path = "/logout/valegames")
+    @PostMapping ("/logout/valegames")
     public ResponseEntity<?> logout(HttpServletRequest request) {
         System.out.println("loggin out");
         try {
@@ -49,10 +51,19 @@ public class AccountController {
         }
     }
 
-    @PostMapping(path = "/login/valegames")
+    @PostMapping("/login/valegames")
     public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest, HttpServletRequest request) {
         try {
             String response = accountService.login(loginRequest, request);
+            System.out.println("login response " + response + " | " + loginRequest.getUsername());
+
+            if (response.equals("register")) {
+                return ResponseEntity.ok(response);
+            }
+            else if (loginRequest.getType().equals("valegames") && !response.equals(loginRequest.getUsername())) {
+                return ResponseEntity.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).body("Invalid username or password");
+            }
+
             return ResponseEntity.ok(response);
         }
         catch (Exception e) {
@@ -60,14 +71,35 @@ public class AccountController {
         }
     }
 
-    @PostMapping(path = "/register/valegames")
-    public ResponseEntity<?> registerUser(@RequestBody LoginRequest loginRequest) {
+    @PostMapping("/register/valegames")
+    public ResponseEntity<?> registerUser(  @RequestParam("username") String username,
+                                            @RequestParam("password") String credential,
+                                            @RequestParam(value = "image", required = false) MultipartFile image,
+                                            @RequestParam("type") String type ) {
+
+        accountService.registerUser(username, credential, image, type);
+        return ResponseEntity.ok("Registered user " + username);
+    }
+
+    @PostMapping("/account/delete")
+    public ResponseEntity<?> deleteUser(@RequestParam String username, HttpServletRequest request) {
+        System.out.println("deleting user now");
         try {
-            String response = accountService.registerUser(loginRequest);
-            return ResponseEntity.ok(response);
+            accountService.deleteUser(username, request);
+            return ResponseEntity.ok("deleted user" + username);
         }
         catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).body("Failed to register");
+            return ResponseEntity.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).body("Failed to delete account");
         }
     }
+
+    @PostMapping("/account/update")
+    public ResponseEntity<?> updateUser(@RequestParam(value = "oldUsername", required = false) String oldUsername,
+                                        @RequestParam(value = "newUsername", required = false) String newUsername,
+                                        @RequestParam(value = "image", required = false) MultipartFile image ) {
+        
+        accountService.updateUser(oldUsername, newUsername, image);
+        return ResponseEntity.ok("updated account");
+    }
+    
 }
