353db6e96c4634879d6683b9a4553178e4456152
VladimirBoro <vborodianski19@gmail.com>
2024-09-06 18:19:10 -0700

    add global exception handler

---
+++ spring/src/main/java/springboot/spring/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package springboot.spring.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import springboot.spring.exceptions.UsernameAlreadyExistsException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    public ResponseEntity<String> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException uae) {
+        return new ResponseEntity<>(uae.getMessage(), HttpStatus.CONFLICT);
+    }
+}
