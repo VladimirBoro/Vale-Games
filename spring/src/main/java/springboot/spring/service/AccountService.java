826e299e0e48d1e700522a9eb8d6ead73b4b3ac9
VladimirBoro <vborodianski19@gmail.com>
2024-09-06 18:23:56 -0700

    account update changes and profile picture stuff

--- spring/src/main/java/springboot/spring/service/AccountService.java
+++ spring/src/main/java/springboot/spring/service/AccountService.java
@@ -1,14 +1,23 @@
 package springboot.spring.service;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
@@ -24,6 +33,7 @@ import jakarta.servlet.http.HttpSession;
 import springboot.spring.entities.Account;
 import springboot.spring.entities.AccountDigest;
 import springboot.spring.entities.AccountOauth2;
+import springboot.spring.exceptions.UsernameAlreadyExistsException;
 import springboot.spring.repo.AccountDigestRepo;
 import springboot.spring.repo.AccountOauth2Repo;
 import springboot.spring.repo.AccountRepo;
@@ -40,6 +50,12 @@ public class AccountService {
 
     @Autowired
     private AccountOauth2Repo accountOauth2Repo;
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @Autowired
+    private ScoreService scoreService;
 
     @Value("${spring.datasource.password}")
     private String CLIENT_ID;
@@ -154,30 +170,52 @@ public class AccountService {
 
         return response;
     }
-
-    public String registerUser(LoginRequest request) {
+    
+    public void registerUser(String username, String credential, MultipartFile image, String type) {
         String response = "";
+        
+        // RETURN SOME TYPE OF ERROR RESPONSE (422?)
+        if (accountRepo.existsByUsername(username)) {
+            System.out.println("user already exists!");
+            throw new UsernameAlreadyExistsException("username " + username + " already exists");
+        }
 
-        System.out.println("TYPE " + request.getType());
-        if (request.getType().equals("google")) {
+        System.out.println("TYPE " + type);
+        if (type.equals("google")) {
             System.out.println("googly");
-            response = googleRegister(request);
+            response = googleRegister(username, credential, image);
         }
         else
         {
             System.out.println("locally");
-            response = valeGamesRegister(request);
+            response = valeGamesRegister(username, credential, image);
         }
 
-        return response;
+        String imageLocation = "";
+        boolean isCustomPic = true;
+        if (image == null) {
+            int defaultPicNumber = (int)(Math.random() * 6) + 1;
+            String basePath = "./spring/src/main/resources/static/default_profile_pictures";
+            isCustomPic = false;
+            imageLocation = basePath + "/default_" + defaultPicNumber + ".png";
+        }
+        else {
+            imageLocation = fileStorageService.storeProfilePicture(username, image);
+        }
+
+        Account account = new Account();
+        account.setUsername(username);
+        account.setAuth_method(type);
+        account.setCreated_at(dateUtil.getDate());
+        account.setProfile_pic(imageLocation);
+        account.setPictureCustom(isCustomPic);
+        accountRepo.save(account);
     }
 
-    private String googleRegister(LoginRequest request) {
+    private String googleRegister(String username, String credential, MultipartFile image) {
         String response = "registered user";
 
         String sub = "";
-        String credential = request.getPassword();
-        String username = request.getUsername();
         
         System.out.println(username + " " + credential);
         try {
@@ -191,16 +229,6 @@ public class AccountService {
             e.printStackTrace();
         }
 
-        if (accountRepo.existsByUsername(username)) {
-            response = "Username already in use";
-        }
-        
-        // register googler user into our database boi
-        Account account = new Account();
-        account.setUsername(username);
-        account.setAuth_method("googler");
-        account.setCreated_at(dateUtil.getDate());
-        accountRepo.save(account);
         
         AccountOauth2 accountOauth2 = new AccountOauth2();
         accountOauth2.setUsername(username);
@@ -210,26 +238,17 @@ public class AccountService {
         return response;
     }
 
-    private String valeGamesRegister(LoginRequest request) {
+    private String valeGamesRegister(String username, String password, MultipartFile image) {
         String response = "registered user";
 
-        String username = request.getUsername();
-        // AccountDigest accountCheck = accountDigestRepo.findByUsername(username);
-        
-        if (accountRepo.existsByUsername(username)) {
-            // username exists! return 422 back
-            response = "Username already exists";
+
+        if (image != null) {
+            System.out.println("(IMAGE) yo im not null!");
         }
         
         // bcrpyt le password
-        String hashedPassword = bcrypt.encode(request.getPassword());
+        String hashedPassword = bcrypt.encode(password);
 
-        Account account = new Account();
-        account.setUsername(username);
-        account.setAuth_method("valegames");
-        account.setCreated_at(dateUtil.getDate());
-        accountRepo.save(account);
-        
         // register user with username and password
         AccountDigest accountDigest = new AccountDigest();
         accountDigest.setUsername(username);
@@ -237,6 +256,131 @@ public class AccountService {
         accountDigestRepo.save(accountDigest);
 
         return response;
+    }
+
+    public ResponseEntity<Resource> getProfilePicture(String username) {
+        Account account = accountRepo.findByUsername(username);
+        String pathToPicture = account.getProfile_pic();
+        
+        try {
+            // Define the path to your image folder
+            Path imagePath = Paths.get(pathToPicture).normalize();
+            Resource resource = new UrlResource(imagePath.toUri());
+
+            if (resource.exists()) {
+                // Set response headers to make the browser treat it as an image file
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    public String deleteUser(String username, HttpServletRequest request) {
+        // logout to destroy session
+        logout(request);
+        System.out.println("deleting " + username);
+        // now delete user info from tables
+        Account account = accountRepo.findByUsername(username);
+        try {
+            if (account.isPictureCustom()) {
+                Path profilePicPath = Paths.get(account.getProfile_pic());
+                Files.deleteIfExists(profilePicPath);
+            }
+
+            if (account.getAuth_method().equals("valegames")) {
+                AccountDigest accountDigest = accountDigestRepo.findByUsername(username);
+                accountDigestRepo.delete(accountDigest);
+            }
+            else {
+                AccountOauth2 accountOauth2 = accountOauth2Repo.findByUsername(username);
+                accountOauth2Repo.delete(accountOauth2);
+            }
+
+            accountRepo.delete(account);
+            return "yuh";
+        }
+        catch (Exception e) {
+            return "error";
+        }
+
+    }
+
+    public String updateUser(String oldUsername, String newUsername, MultipartFile image) {
+        if (!oldUsername.equals(newUsername)) {
+            System.out.println("updating username... " + oldUsername + " " + newUsername);
+            boolean updatingImage = image == null ? false : true;
+
+            updateUsername(oldUsername, newUsername, updatingImage); 
+        }
+        
+        if (image != null) {
+            System.out.println("updating imagey...");
+            
+            if (newUsername.equals(oldUsername) || newUsername == null || newUsername.equals("")) {
+                System.out.println("old image update");
+                updateProfilePicture(oldUsername, image);
+            }
+            else {
+                System.out.println("new image update");
+                updateProfilePicture(newUsername, image);
+            }
+        }
+        
+        return "success updating account";
+    }
+
+    private void updateUsername(String oldUsername, String newUsername, boolean updatingImage) {
+        Account exisitingAccount = accountRepo.findByUsername(newUsername);
+        if (exisitingAccount != null) {
+            System.out.println("ballin!");
+            throw new UsernameAlreadyExistsException("Username " + newUsername + " is taken.");
+        }
+        
+        // handle associated data too, so all of the datatables and shiet
+        Account account = accountRepo.findByUsername(oldUsername);
+        account.setUsername(newUsername);
+        accountRepo.save(account);
+        
+        if (account.getAuth_method().equals("valegames")) {
+            AccountDigest accountDigest = accountDigestRepo.findByUsername(oldUsername);
+            accountDigest.setUsername(newUsername);
+            accountDigestRepo.save(accountDigest);
+        }
+        else {
+            AccountOauth2 accountOauth2 = accountOauth2Repo.findByUsername(oldUsername);
+            accountOauth2.setUsername(newUsername);
+            accountOauth2Repo.save(accountOauth2);
+        }
+        
+        scoreService.updateUsername(oldUsername, newUsername);
+        
+        // update filename here!
+        if (account.isPictureCustom() || updatingImage) {
+            // update name
+            String currentPicturePath = account.getProfile_pic();
+            String imageExtension = currentPicturePath.substring(currentPicturePath.lastIndexOf("."));
+            String newName = newUsername + "_profilePic" + imageExtension;
+
+            String basePath = currentPicturePath.substring(0, currentPicturePath.lastIndexOf("\\"));
+            String newPicturePath = basePath + "\\" + newName;
+
+            File oldPicture = new File(currentPicturePath);
+            File newPicture = new File(newPicturePath);
+            System.out.println(newPicturePath + " " + newPicture);
+            oldPicture.renameTo(newPicture);
+        }
+    }
+
+    private void updateProfilePicture(String username, MultipartFile image) {
+        Account account = accountRepo.findByUsername(username);
+        account.setPictureCustom(true);
+        account.setProfile_pic(fileStorageService.storeProfilePicture(username, image));
+        accountRepo.save(account);
     }
 
 }
---
+++ spring/src/main/java/springboot/spring/service/FileStorageService.java
@@ -0,0 +1,39 @@
+package springboot.spring.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileStorageService {
+    public String storeProfilePicture(String username, MultipartFile file) {
+        return storeProfilePictureLocally(username, file);
+    }
+
+    public String storeProfilePictureLocally(String username, MultipartFile file) {
+        String type = file.getContentType().split("/")[1];
+        String filename = username + "_profilePic." + type;
+        System.out.println("FILENAME: " + filename + " ");
+
+        try {
+            Path uploadDirectory = Paths.get("./spring/src/main/java/springboot/spring/uploads/profile_pictures");
+            Path targetLocation = uploadDirectory.resolve(filename);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return targetLocation.toString();
+        } 
+        catch (Exception e) {
+            throw new RuntimeException("Could not store file locally...");
+        }
+    }
+
+    public void updateUsername(String newUsername, String oldfilePath) {
+
+    }
+
+    // we gonna need a function to store pic in CDN or something later too.
+}
