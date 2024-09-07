c56c4ec4d84d0da72ba9e8f20dbc523db2fab5f4
VladimirBoro <vborodianski19@gmail.com>
2024-09-06 18:17:11 -0700

    handle account updates

--- vale-games/src/pages/account/Account.js
+++ vale-games/src/pages/account/Account.js
@@ -3,39 +3,61 @@ import Leaderboard from "../../components/leaderboard/Leaderboard";
 import customAxios from "../../util/customAxios";
 import styles from "./account.module.css";
 import { updateProfile } from "../../util/restful";
+import { useNavigate } from "react-router-dom";
+import { fetchProfilePic } from "../../util/restful";
+// import AccountForm from "./components/AccountForm"
 
 function Account() {
+    const navigate = useNavigate(); 
+
     const URL = process.env.REACT_APP_SERVER_URL;
     const PATH = process.env.REACT_APP_PLAYER_SCORES;
 
     const [editing, setEditing] = useState(false);
     const [username, setUsername] = useState(localStorage.getItem("user"));
     const [image, setImage] = useState(null);
-
-    const fetchHighScores = async() => {
-        await customAxios.get(URL,
-            {
-                withCredentials: true
-            }
-        )
-        .then(response => {
-            console.log(response);
-        })
-    }
+    const [profilePicPreview, setProfilePicPreview] = useState(null);
 
     // fetch scores
     useEffect(() => {
-        fetchHighScores();
+        setProfilePicPreview(localStorage.getItem("profilePic"));
     }, [])
 
     const editProfile = () => {
+        setProfilePicPreview(localStorage.getItem("profilePic"));
         setEditing(!editing);
+    }
+
+    const deleteProfile = async () => {
+        console.log("deleting user...", username);
+
+        await customAxios.post("/account/delete", null, {
+            params: {username: username}
+        })
+        .then(response => console.log(response.data))
+        .catch(err => console.log(err));
+
+        navigate("/");
+        // localStorage.removeItem("user");
+        localStorage.clear();
+        window.dispatchEvent(new Event("storage"));
     }
 
     const handleFileChange = (e) => {
         if (e.target.files) {
             setImage(e.target.files[0]);
         }
+
+        const profilePic = new FileReader();
+
+        profilePic.onloadend = () => {
+            const base64profilePic = profilePic.result;
+            // localStorage.setItem("profilePic",  base64profilePic);
+            setProfilePicPreview(base64profilePic);
+            window.dispatchEvent(new Event("storage"));
+        }
+
+        profilePic.readAsDataURL(e.target.files[0]);
     }
 
     const handleUsernameChange = (e) => {
@@ -44,8 +66,56 @@ function Account() {
     }
 
     const handleUpdate = async () => {
-        await updateProfile(image, username);
+        // await updateProfile(image, username);
+        const currentUsername = localStorage.getItem("user");
+        if (currentUsername === username && image === null) {
+            console.log("u gotta update to update mang");
+            return;
+        }
 
+        console.log(localStorage.getItem("user"));
+        const formData = new FormData();
+        formData.append("oldUsername", localStorage.getItem("user"));
+        formData.append("newUsername", username);
+        formData.append("image", image);
+
+        // update account
+        console.log("posting update now!");
+        customAxios.post("/account/update", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        })
+        .then(response => {
+            console.log("we good on update", response);
+            localStorage.setItem("user", username);
+        })
+        .catch(err => {
+            // an error from username already taken should be a thing here
+            console.log("error::", err);
+        });
+        
+        // fetch the pic
+        console.log("fetching after the update");
+        localStorage.removeItem("profilePic");
+        await fetchProfilePic(localStorage.getItem("user"))
+        
+        // localStorage.setItem("profilePic", image);
+        try {
+            const profilePic = new FileReader();
+            profilePic.onloadend = () => {
+                const base64profilePic = profilePic.result;
+                localStorage.setItem("profilePic",  base64profilePic);
+                window.dispatchEvent(new Event("storage"));
+    
+            }
+            profilePic.readAsDataURL(image);
+            window.dispatchEvent(new Event("storage"));
+        }
+        catch (err) {
+            console.log(err);
+        }
+        
         setEditing(!editing);
     }
 
@@ -63,36 +133,34 @@ function Account() {
         <div className={styles.page}>
             {editing ? (
                 <div className={styles.form}>
-                    <div className={styles.profilePic}>
-                        <img alt="User profile picture."/>
-                        <input type="file" onChange={handleFileChange} accept="image/png, image/jpeg"/>
-                    </div>
+                    <img src={profilePicPreview} className={styles.profilePic} alt="User profile picture."/>
+                    <input type="file" onChange={handleFileChange} accept="image/png, image/jpeg"/>    
+                    
                     <div>
                         <label htmlFor="username">Username:</label>
-                        <input  name="username" onChange={handleUsernameChange} defaultValue={localStorage.getItem("user")}/>
+                        <input  name="username" onChange={handleUsernameChange} defaultValue={localStorage.getItem("user")}  id={styles.username}/>
                         <p>Account created: whenever</p>
-                        <button onClick={handleUpdate}>submit</button>
-                        <button onClick={editProfile}>cancel</button>
+                        <button onClick={handleUpdate} className={styles.button}>submit</button>
+                        <button onClick={editProfile} className={styles.button}>cancel</button>
                     </div>
-                    {/* <input type="file" accept="image/png, image/jpeg"/> */}
                 </div>
             ) : (
                 <div className={styles.form}>
                     <div>
-                        <img alt="User profile picture."/>
+                        <img src={localStorage.getItem("profilePic")} className={styles.profilePic} alt="User profile picture."/>
                     </div>
                     <div>
                         <p>Username: {localStorage.getItem("user")}</p>
                         <p>Account created: whenever</p>
-                        <button onClick={editProfile}>edit profile</button>
                     </div>
+
+                    <div> 
+                        <button onClick={editProfile} className={styles.button}>edit profile</button>
+                        <button onClick={deleteProfile} className={styles.button}>delete profile</button>
+                    </div>
+
                 </div>   
             )}
-
-            {/* highscores */}
-            <div> 
-                {/* <Leaderboard data={} printRow={printRow} metric={"Score"}></Leaderboard> */}
-            </div>
         </div>
     );
 }
