d81389e10853d16b0153550b22e8f9f76f1d6e82
VladimirBoro <vborodianski19@gmail.com>
2024-09-06 18:15:36 -0700

    register fixes

--- vale-games/src/pages/register/Register.js
+++ vale-games/src/pages/register/Register.js
@@ -1,41 +1,74 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import axios from 'axios';
-import login from "../login/login.module.css"
+import login from "../login/login.module.css";
+import styles from "./register.module.css";
+import ConfirmPassword from "./ConfirmPassword";
+import customAxios from "../../util/customAxios";
+import { useNavigate } from "react-router-dom";
 
 
-const Register = () => {
+
+function Register() {
     const URL = process.env.REACT_APP_SERVER_URL;
     const REGISTER_PATH = process.env.REACT_APP_REGISTER_VALEGAMES_PATH;
+    const navigate = useNavigate();
 
-    const [usernameError, setUsernameError] = React.useState(false);
+    const [usernameError, setUsernameError] = useState(false);
+    const [matching, setMatching] = useState(true);
+    const [image, setImage] = useState(null);
+    const [preview, setPreview] = useState(null);
 
-    const usernameRef = React.useRef();
-    const passwordRef = React.useRef();
+
+    const usernameRef = useRef("");
+    const passwordRef = useRef("");
+    const confirmRef = useRef("");
 
     const registerUser = (event) => {
         event.preventDefault();
-
         const username = usernameRef.current.value;
         const password = passwordRef.current.value;
+        const confirm = confirmRef.current.value;
+        const type = "valegames";
 
-        axios.post(URL + REGISTER_PATH,
-            {
-                username: username,
-                password: password,
-                type: "valegames"
-            },
-            {
-                withCredentials: true
+        const formData = new FormData();
+        formData.append("username", username);
+        formData.append("password", password);
+        formData.append("image", image);
+        formData.append("type", type);
+
+        setMatching(true);
+        if (password !== confirm) {
+            setMatching(false);
+            return;
+        }
+
+        customAxios.post(REGISTER_PATH, formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
             }
         )
-        .then(request => {
+        .then(response => {
             setUsernameError(false);
-            console.log("ramadan challenge!", request.data);
+            navigate("/login");
+            console.log("ramadan challenge!", response.data);
         })
         .catch(error => {
-            setUsernameError(true);
             console.log("HAHAHA", error);
+            setUsernameError(true);
         });
+    }
+
+    const handleFileChange = (e) => {
+        if (e.target.files) {
+            setImage(e.target.files[0]);
+
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPreview(reader.result); // Set the image preview URL
+            };
+            reader.readAsDataURL(e.target.files[0]); // Read the file as a data URL
+        }
     }
 
     return (
@@ -49,11 +82,23 @@ const Register = () => {
                         <></>
                     )}
                     <form onSubmit={registerUser} className={login.form}>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" name="username" ref={usernameRef} required/>
+                        <div className={login.form}>
+                            <label htmlFor="username">Username*</label>
+                            <input type="text" name="username" ref={usernameRef} required/>
 
-                        <label htmlFor="password">Password</label>
-                        <input type="password" ref={passwordRef} required/>
+                            <ConfirmPassword matching={matching}/>
+
+                            <label htmlFor="password">Password*</label>
+                            <input type="password" ref={passwordRef} required/>
+
+                            <label htmlFor="password">Confirm Password*</label>
+                            <input type="password" ref={confirmRef} required/>
+                        </div>
+                        <div>
+                            <p>Profile Picture</p>
+                            <img src={preview} alt="dope profile pic" className={styles.preview}/>
+                            <input type="file" onChange={handleFileChange} accept="image/png, image/jpeg"/>
+                        </div>
 
                         <button type="submit">Register</button>
                     </form>
--- vale-games/src/pages/register/RegisterGoogler.js
+++ vale-games/src/pages/register/RegisterGoogler.js
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import axios from 'axios';
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import login from "../login/login.module.css"
+import styles from "../login/login.module.css"
+import customAxios from "../../util/customAxios";
 
 const RegisterGoogler = () => {
-    const URL = process.env.REACT_APP_SERVER_URL;
-    const REG_URL = process.env.REACT_APP_REGISTER_GOOGLE_PATH;
     const REGISTER_PATH = process.env.REACT_APP_REGISTER_VALEGAMES_PATH;
-
 
     const location = useLocation();
     const navigate = useNavigate();
@@ -16,21 +14,24 @@ const RegisterGoogler = () => {
 
     const [username, setUsername] = useState(""); 
     const [usernameError, setUsernameError] = useState(false);
+    const [image, setImage] = useState(null);
+    const [preview, setPreview] = useState(null);
 
     function registerUser(event) {
         event.preventDefault();
         console.log(username, credential);
+
+        const formData = new FormData();
+        formData.append("username", username);
+        formData.append("password", credential);
+        formData.append("image", image);
+        formData.append("type", "google");
         
-        axios.post(URL + REGISTER_PATH,
-            {
-                password: credential,
-                username: username,
-                type: "google"
-            },
-            {
-                withCredentials: true
+        customAxios.post(REGISTER_PATH, formData, {
+            headers: {
+                "Content-Type": "multipart/form-data"
             }
-        )
+        })
         .then(res => {
             console.log("Successful goog register:", res.data);
             setUsernameError(false);
@@ -46,17 +47,29 @@ const RegisterGoogler = () => {
         setUsername(event.target.value);
     }
 
+    const handleFileChange = (e) => {
+        if (e.target.files) {
+            setImage(e.target.files[0]);
+
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPreview(reader.result); // Set the image preview URL
+            };
+            reader.readAsDataURL(e.target.files[0]); // Read the file as a data URL
+        }
+    }
+
     return (
-        <div className={login.page}>
-            <div className={login.signIn}>
+        <div className={styles.page}>
+            <div className={styles.signIn}>
                 <h1>Register your username!</h1>
-                <div className={login.formContainer}>
+                <div className={styles.formContainer}>
                     { usernameError ? (
                         <p>USERNAME ALREADY IN USE</p>
                     ) : (
                         <></>
                     )}
-                    <form className={login.form} onSubmit={registerUser}>
+                    <form className={styles.form} onSubmit={registerUser}>
                         <label htmlFor="username">Username</label>
                         <input 
                             type="text" 
@@ -65,6 +78,11 @@ const RegisterGoogler = () => {
                             onInput={handleChange} 
                             required
                         />
+                        <div>
+                            <p>Profile Picture</p>
+                            <img src={preview} alt="dope profile pic" className={styles.preview}/>
+                            <input type="file" onChange={handleFileChange} accept="image/png, image/jpeg"/>
+                        </div>
                         <button type="submit">Register</button>
                     </form>
                 </div>
