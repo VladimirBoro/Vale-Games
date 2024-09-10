92da806119b79fb6930251987c4d22ff38074920
VladimirBoro <vborodianski19@gmail.com>
2024-09-10 15:05:28 -0700

    updated home page images

--- vale-games/src/pages/home/Home.js
+++ vale-games/src/pages/home/Home.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./home.module.css";
 import image from "./background.png";
+import snakeImage from "./images/snake.png";
+import minesweeperImage from "./images/minesweeper.png";
+import froggerImage from "./images/frogger.png";
+import cardmatchImage from "./images/cardmatch.png";
+import skyImage from "./images/sky.png";
 
 const Home = () => {
     const URL = process.env.REACT_APP_SERVER_URL;
@@ -11,16 +16,16 @@ const Home = () => {
             <ul className={styles.gamesDisplay}>
                 <li>
                     <div className={styles.gameSelection}>
-                        <a href="/game/frogger" className={styles.anchor}>
-                            <img src={image} className={styles.homeImage} alt="dope ass pic"/>
+                        <a href="/game/snake" className={styles.anchor}>
+                            <img src={snakeImage} className={styles.homeImage} alt="dope ass pic"/>
                         </a>
                         <Link to="/game/snake">Snake</Link>
                     </div>    
                 </li>    
                 <li>    
                     <div className={styles.gameSelection}>
-                        <a href="/game/frogger" className={styles.anchor}>
-                            <img src={image} className={styles.homeImage} alt="dope ass pic"/>
+                        <a href="/game/minesweeper" className={styles.anchor}>
+                            <img src={minesweeperImage} className={styles.homeImage} alt="dope ass pic"/>
                         </a>
                         <Link to="/game/minesweeper">Minesweeper</Link>
                     </div>
@@ -28,33 +33,33 @@ const Home = () => {
                 <li>    
                     <div className={styles.gameSelection}>
                         <a href="/game/frogger" className={styles.anchor}>
-                            <img src={image} className={styles.homeImage} alt="dope ass pic"/>
+                            <img src={froggerImage} className={styles.homeImage} alt="dope ass pic"/>
                         </a>
                         <Link to="/game/frogger">Frogger</Link>
                     </div>
                 </li>    
                 <li>    
                     <div className={styles.gameSelection}>
-                        <a href="/game/frogger" className={styles.anchor}>
-                            <img src={image} className={styles.homeImage} alt="dope ass pic"/>
+                        <a href="/game/cardmatch" className={styles.anchor}>
+                            <img src={cardmatchImage} className={styles.homeImage} alt="dope ass pic"/>
                         </a>
                         <Link to="/game/cardmatch">Cardmatch</Link>
                     </div>
                 </li>    
                 <li>    
                     <div className={styles.gameSelection}>
-                        <a href="/game/frogger" className={styles.anchor}>
-                            <img src={image} className={styles.homeImage} alt="dope ass pic"/>
+                        <a href="/game/birdyflap" className={styles.anchor}>
+                            <img src={skyImage} className={styles.homeImage} alt="dope ass pic"/>
                         </a>
                         <Link to="/game/birdyflap">Birdy Flap</Link>
                     </div>
                 </li>    
                 <li>    
                     <div className={styles.gameSelection}>
-                        <a href="/game/frogger" className={styles.anchor}>
+                        <a href="/game/birdyflap" className={styles.anchor}>
                             <img src={image} className={styles.homeImage} alt="dope ass pic"/>
                         </a>
-                        <Link to="/game/birdyflap">Birdy Flap</Link>
+                        <Link to="/game/birdyflap">Unicorn Dash</Link>
                     </div>
                 </li>    
             
