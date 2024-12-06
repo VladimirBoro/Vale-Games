7e1e67eed584d2e154980ed2cf569835c08366e1
VladimirBoro <vborodianski19@gmail.com>
2024-12-05 18:16:30 -0800

    refactor account and add delete account confirmation page

--- vale-games/src/pages/account/Account.js
+++ vale-games/src/pages/account/Account.js
@@ -1,161 +1,53 @@
 import React, { useEffect, useState } from "react";
-import Leaderboard from "../../components/leaderboard/Leaderboard";
-import customAxios from "../../util/customAxios";
 import styles from "./account.module.css";
-import { fetchMemberSince, updateProfile } from "../../util/restful";
-import { useNavigate } from "react-router-dom";
-import { fetchProfilePic } from "../../util/restful";
-import  TakenUsername  from "./components/TakenUsername";
+import { fetchMemberSince, fetchProfilePic } from "../../util/restful";
+import { ACCOUNT_PAGE_STATE } from "./constants";
+import Default from "./components/Default";
+import Edit from "./components/Edit";
+import Delete from "./components/Delete";
 
 function Account() {
-    const navigate = useNavigate(); 
-
-    const [editing, setEditing] = useState(false);
-    const [username, setUsername] = useState(localStorage.getItem("user"));
-    const [image, setImage] = useState(null);
-    const [profilePicPreview, setProfilePicPreview] = useState(null);
-    const [isProfilePicChanged, setIsProfilePicChanged] = useState(false);
-    const [takenUsername, setTakenUsername] = useState(false);
+    const [pageState, setPageState] = useState(ACCOUNT_PAGE_STATE.DEFAULT);
+    const [profilePic, setProfilePic] = useState(null);
     const [memberSince, setMemberSince] = useState("");
 
     useEffect(() => {
         const fetchPreview = async () => {
-            setProfilePicPreview(await fetchProfilePic(username));
-            setMemberSince(await fetchMemberSince(username));
+            setProfilePic(await fetchProfilePic(localStorage.getItem("user")));
+            setMemberSince(await fetchMemberSince(localStorage.getItem("user")));
         }
 
         // clear game title in header upon opening page
         localStorage.setItem("currentGame", "");
         window.dispatchEvent(new Event("game"));
         
-        setTakenUsername(false);
         fetchPreview();
     }, [])
 
-    const editProfile = async () => {
-        if (editing && isProfilePicChanged) {
-            setProfilePicPreview(await fetchProfilePic(username));
-            setIsProfilePicChanged(false);
+
+    // callback that will be passed down to state components
+    const changeState = async (mode) => {
+        if (mode == ACCOUNT_PAGE_STATE.DEFAULT) {
+            setProfilePic(await fetchProfilePic(localStorage.getItem("user")));
         }
 
-        setEditing(!editing);
+        setPageState(mode);
     }
 
-    const deleteProfile = async () => {
-        console.log("deleting user...", username);
-
-        await customAxios.post("/account/delete", null, {
-            params: {username: username}
-        })
-        .then(response => console.log(response.data))
-        .catch(err => console.log(err));
-
-        navigate("/");
-        localStorage.clear();
-        window.dispatchEvent(new Event("storage"));
-    }
-
-    const handleFileChange = async (e) => {
-        if (e.target.files.length != 0) {
-            setIsProfilePicChanged(true);
-            setImage(e.target.files[0]);
+    const renderForm = () => {
+        switch (pageState) {
+            case ACCOUNT_PAGE_STATE.DEFAULT:
+                return <Default changeState={changeState} profilePic={profilePic} memberSince={memberSince} />
+            case ACCOUNT_PAGE_STATE.EDIT:
+                return <Edit changeState={changeState} profilePic={profilePic} memberSince={memberSince}/>
+            case ACCOUNT_PAGE_STATE.DELETE:
+                return <Delete changeState={changeState}/>
         }
-        else {
-            setImage(null);
-            setProfilePicPreview(await fetchProfilePic(username));
-            return;
-        }
-
-        const profilePic = new FileReader();
-
-        profilePic.onloadend = () => {
-            const base64profilePic = profilePic.result;
-            setProfilePicPreview(base64profilePic);
-        }
-
-        profilePic.readAsDataURL(e.target.files[0]);
-    }
-
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
-    }
-
-    const handleUpdate = async () => {
-        const currentUsername = localStorage.getItem("user");
-        if (currentUsername === username && image === null) {
-            console.log("u gotta update to update mang");
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append("oldUsername", localStorage.getItem("user"));
-        formData.append("newUsername", username);
-        formData.append("image", image);
-
-        // update account
-        console.log("posting update now!");
-        try {
-            await customAxios.post("/account/update", formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data"
-                }
-            });
-
-            if (username !== "") {
-                localStorage.setItem("user", username);
-            }
-        }
-        catch (err) {
-            console.log("error", err);
-            setTakenUsername(true);
-            return;
-        }
-        
-        // set the pic
-        console.log("fetching after the update");
-        setImage(profilePicPreview);
-        setIsProfilePicChanged(false);
-        
-        window.dispatchEvent(new Event("storage"));
-        window.dispatchEvent(new Event("profilePic"));
-
-        setEditing(!editing);
-        setTakenUsername(false);
     }
 
     return (
         <div className={styles.page}>
-            {editing ? (
-                <div className={styles.form}>
-                    <img src={profilePicPreview} className={styles.profilePic} alt="User profile picture."/>
-                    <input type="file" onChange={handleFileChange} accept="image/png, image/jpeg"/>    
-                    
-                    <div>
-                        <TakenUsername taken={takenUsername}/>
-                        <label htmlFor="username">Username:</label>
-                        <input  name="username" onChange={handleUsernameChange} defaultValue={localStorage.getItem("user")}  id={styles.username}/>
-                        <p>Account created: {memberSince}</p>
-                        <button onClick={handleUpdate} className={styles.button}>submit</button>
-                        <button onClick={editProfile} className={styles.button}>cancel</button>
-                    </div>
-                </div>
-            ) : (
-                <div className={styles.form}>
-                    <div>
-                        <img src={profilePicPreview} className={styles.profilePic} alt="User profile picture."/>
-                    </div>
-                    <div>
-                        <p>Username: {localStorage.getItem("user")}</p>
-                        <p>Account created: {memberSince}</p>
-                    </div>
-
-                    <div> 
-                        <button onClick={editProfile} className={styles.button}>edit profile</button>
-                        <button onClick={deleteProfile} className={styles.button}>delete profile</button>
-                    </div>
-
-                </div>   
-            )}
+            {renderForm()}
         </div>
     );
 }
---
+++ vale-games/src/pages/account/constants.js
@@ -0,0 +1,5 @@
+export const ACCOUNT_PAGE_STATE = {
+    DEFAULT: 0,
+    EDIT: 1,
+    DELETE: 2
+}
