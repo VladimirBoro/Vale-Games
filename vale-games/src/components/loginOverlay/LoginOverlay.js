37a6233051078a4fcbd132aa39cd839bc9ec1904
VladimirBoro <vborodianski19@gmail.com>
2024-12-06 13:15:33 -0800

    env var rename

--- vale-games/src/components/loginOverlay/LoginOverlay.js
+++ vale-games/src/components/loginOverlay/LoginOverlay.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import VgRegisterForm from "./register/VgRegisterForm";
 import LoginForm from "./login/LoginForm";
 import GoogleRegisterForm from "./register/GoogleRegisterForm";
--- vale-games/src/index.js
+++ vale-games/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
